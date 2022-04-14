50dba2d6e40ac3fd7970ec643b277a0fb9dd72aa
Matt Hopkins <moebaca@hotmail.com>
2022-04-14 07:58:05 -0700

    SYSINIT

---
+++ test/hopkins-resume.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as HopkinsResume from '../lib/hopkins-resume-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/hopkins-resume-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new HopkinsResume.HopkinsResumeStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
