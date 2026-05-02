b337d30bb2e9de748f362857827f805fe4c682a5
Matthew Hopkins <matthew.hopkins@paypay-corp.co.jp>
2026-05-02 19:36:50 +0900

    Update resume with PayPay experience and modernize stack
    - Add PayPay Sr. Platform Engineer role (2025 - Present)
    - Update Autify dates to 2022 - 2025
    - Rewrite summary, skills, and interests sections
    - Replace deprecated CDK constructs (OAI -> OAC, DnsValidatedCertificate -> Certificate)
    - Bump all dependencies to current versions (CDK 2.252, TS 5.7, Jest 29)
    - Remove jQuery, add Google Fonts import for Roboto, add OG meta tags
    - Replace error page with styled 404
    - Rewrite main.js as vanilla JS (6 lines vs 55)
    - Update tests for new CDK patterns
    - Update CI to Node 20

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,23 +1,18 @@
 module.exports = {
-    env: {
-      browser: true,
-      es2021: true,
-      jquery: true,
-    },
-    extends: "eslint:recommended",
-    parserOptions: {
-      ecmaVersion: 12,
-      sourceType: "script",
-    },
-    rules: {
-      indent: ["error", 2],
-      "linebreak-style": ["error", "unix"],
-      quotes: ["error", "single"],
-      semi: ["error", "always"],
-      "no-unused-vars": "warn",
-    },
-    globals: {
-      $: "readonly",
-      jQuery: "readonly",
-    },
-  };
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: 'eslint:recommended',
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'script',
+  },
+  rules: {
+    indent: ['error', 2],
+    'linebreak-style': ['error', 'unix'],
+    quotes: ['error', 'single'],
+    semi: ['error', 'always'],
+    'no-unused-vars': 'warn',
+  },
+};
